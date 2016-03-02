21f81ed58cb314053780805cf8d8592b0c840143
yannie <yannie1308@yahoo.com>
2016-03-03 01:18:08 +0700

    menambahkan script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+﻿function readMore(){
+	
+		var temp = document.getElementById("readMore").style.display;
+		if(temp=="none")
+		
+			document.getElementById("readMore").style.display="block";
+			
+		else
+		
+			document.getElementById("readMore").style.display="none"
+		
+}
+
+function readMore2(){
+	
+		var temp = document.getElementById("readMore2").style.display;
+		if(temp=="none")
+		
+			document.getElementById("readMore2").style.display="block";
+			
+		else
+		
+			document.getElementById("readMore2").style.display="none"
+		
+}
