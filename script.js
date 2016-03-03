e202dc0c83bb06a360ec02c7ee1ea626d280eca2
yannie <yannie1308@yahoo.com>
2016-03-03 19:08:06 +0700

    merubah kuliner_project
    merubah review_style.css, review.html, script.js dan contact_us.htmn

--- script.js
+++ script.js
@@ -23,3 +23,66 @@ function readMore2(){
 			document.getElementById("readMore2").style.display="none"
 		
 }
+
+
+function readMore3(){
+	
+		var temp = document.getElementById("readMore3").style.display;
+		if(temp=="none")
+		
+			document.getElementById("readMore3").style.display="block";
+			
+		else
+		
+			document.getElementById("readMore3").style.display="none"
+		
+}
+
+
+function readMore4(){
+	
+		var temp = document.getElementById("readMore4").style.display;
+		if(temp=="none")
+		
+			document.getElementById("readMore4").style.display="block";
+			
+		else
+		
+			document.getElementById("readMore4").style.display="none"
+		
+}
+
+
+
+function readMore5(){
+	
+		var temp = document.getElementById("readMore5").style.display;
+		if(temp=="none")
+		
+			document.getElementById("readMore5").style.display="block";
+			
+		else
+		
+			document.getElementById("readMore5").style.display="none"
+		
+}
+
+
+function readMore6(){
+	
+		var temp = document.getElementById("readMore6").style.display;
+		if(temp=="none")
+		
+			document.getElementById("readMore6").style.display="block";
+			
+		else
+		
+			document.getElementById("readMore6").style.display="none"
+		
+}
+
+
+
+
+
+
